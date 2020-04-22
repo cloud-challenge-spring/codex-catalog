6978278c27e7dd10349037ca8df8134f1cf4a82c
gfortin <gfortin@keyneosoft.fr>
2020-04-22 15:10:24 +0200

    #1 Register to eureka server

--- src/main/kotlin/org/cloudchallenge/codex/catalog/CatalogApplication.kt
+++ src/main/kotlin/org/cloudchallenge/codex/catalog/CatalogApplication.kt
@@ -3,9 +3,11 @@ package org.cloudchallenge.codex.catalog
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 class CatalogApplication
 
 fun main(args: Array<String>) {
